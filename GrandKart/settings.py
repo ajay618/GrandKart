b176df9c172ce304a13ab0a84fa0a3105e297a38
Ajay Joy <ajayjoy618@gmail.com>
2024-01-20 12:49:15 +0530

    rds databsee chnges

--- GrandKart/settings.py
+++ GrandKart/settings.py
@@ -96,8 +96,12 @@ AUTH_USER_MODEL = 'accounts.Account'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'grandkartdb',
+        'USER': 'mysuperuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'grandkartdatabase.c1ew4s40cwq9.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
